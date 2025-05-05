7ff21bb9f036668b9ba5cbadc58e1ec88174b998
tomo-x7 <158121497+tomo-x7@users.noreply.github.com>
2025-05-06 00:46:43 +0900

    クライアントを分類し実装を整理

--- readme.md
+++ readme.md
@@ -9,14 +9,16 @@ Jetstreamをプロキシするサーバーです。
 1. `ws://localhost:8000`に接続する
 ## 設定
 `.env`ファイルに書く
-|項目|説明|
-|-|-|
-|`wantedCollection`|必要な`collection`をJSON配列形式で記述|
-|`wsURL`|`Jetstream`のURL|
-|`port`|サーバーを建てるポート番号|
+|項目|説明|デフォルト値|
+|-|-|-|
+|`wantedCollections`|必要な`collection`をJSON配列形式で記述※|
+|`wsURL`|`Jetstream`のURL|`wss://jetstream2.us-west.bsky.network/subscribe`|
+|`port`|サーバーを建てるポート番号|`8000`|  
+
+※:必ず指定してください。空配列は今の所エラーになります。理由は、JetstreamのwantedCollections未指定時に全コレクションが取得される仕様に追従するのが面倒だったからです。要望があれば実装します。PRも歓迎です。
 ## 対応しているJetstream引数
-- wantedCollection
-    - .`env`で指定した`wantedCollection`に含まれるもののみ使用可能
+- wantedCollections
+    - .`env`で指定した`wantedCollections`に含まれるもののみ使用可能
     - それ以外が含まれる場合はエラーを返します
     - 接続URLで指定しない場合はサーバー側で設定したすべてになります
 それ以外の引数をURLに含めても何も起こりません。また、サーバーに対し何かを送っても何も起こりません。
--- src/main.ts
+++ src/main.ts
@@ -1,70 +1,93 @@
+import { exit } from "node:process";
 import { type AccountEvent, type CommitEvent, type IdentityEvent, Jetstream } from "@skyware/jetstream";
 import dotenv from "dotenv";
 import WebSocket, { WebSocketServer } from "ws";
 import type { BufferLike, configtype } from "./types.js";
 dotenv.config();
 
+const wantedCollections = process.env.wantedCollections
+	? new Set<string>(JSON.parse(process.env.wantedCollections))
+	: undefined;
+if (wantedCollections == null) {
+	console.error("wantedCollectionsが未指定です(詳細はReadmeを参照)");
+	exit(1);
+}
 const config: configtype = {
 	wsURL: process.env.wsURL ?? "wss://jetstream2.us-west.bsky.network/subscribe",
-	wantedCollections: new Set(JSON.parse(process.env.wantedCollections ?? "[]")),
+	wantedCollections,
 	port: Number.parseInt(process.env.port ?? "8000"),
 };
 
 const server = new WebSocketServer({ port: config.port });
-const connections: Map<string, Set<WebSocket>> = new Map();
-for (const collection of config.wantedCollections) {
-	connections.set(collection, new Set());
-}
+// コレクションを指定せずに接続しているコネクション
+const allSubscriber = new Set<WebSocket>();
+// コレクションを指定して接続しているコネクション
+const specificSubscriver = new Map<string, Set<WebSocket>>(
+	config.wantedCollections == null
+		? undefined
+		: // 空のSetで初期化
+			Array.from(config.wantedCollections).map((collection) => [collection, new Set<WebSocket>()]),
+);
 const send = (data: BufferLike, collection?: string) => {
 	const sent = new Set<WebSocket>();
+	const sendInner = (connection: WebSocket, context: "notCommitEvent" | "allSubscriber" | "specificSubscriber") => {
+		if (!sent.has(connection)) {
+			connection.send(data);
+			sent.add(connection);
+		} else {
+			// never happen
+			console.error(`duplicate connection context:${context}`);
+		}
+	};
 	if (collection == null) {
+		// すべてのクライアントに送信(Commit以外のイベント)
 		for (const connection of server.clients) {
-			if (!sent.has(connection)) {
-				connection.send(data);
-				sent.add(connection);
-			} else {
-				// never happen
-				console.error("duplicate connection(all collections)");
-			}
+			sendInner(connection, "notCommitEvent");
 		}
 	} else {
-		for (const connection of connections.get(collection)?.values() ?? []) {
-			if (!sent.has(connection)) {
-				connection.send(data);
-				sent.add(connection);
-			} else {
-				// never happen
-				console.error("duplicate connection(collection specific)");
-			}
+		// すべてを受け取るクライアントに送信
+		for (const connection of allSubscriber) {
+			sendInner(connection, "allSubscriber");
+		}
+		// 特定のコレクションを受け取るクライアントに送信
+		for (const connection of specificSubscriver.get(collection) ?? []) {
+			sendInner(connection, "specificSubscriber");
 		}
 	}
 };
 server.on("connection", (connection, req) => {
 	console.log("client connect");
 	const { searchParams } = new URL(req.url ?? "/", "wss://ws.url");
-	const wantedCollections = searchParams.has("wantedCollections")
-		? new Set(searchParams.getAll("wantedCollections"))
-		: config.wantedCollections;
-	const cleanup = () => {
+	let cleanup: () => void;
+	if (searchParams.has("wantedCollections")) {
+		// wantedCollectionsが指定されている場合
+		const wantedCollections = new Set(searchParams.getAll("wantedCollections"));
+		cleanup = () => {
+			for (const collection of wantedCollections) {
+				specificSubscriver.get(collection)?.delete(connection);
+			}
+		};
 		for (const collection of wantedCollections) {
-			connections.get(collection)?.delete(connection);
+			const result = specificSubscriver.get(collection)?.add(connection);
+			if (result == null) {
+				cleanup();
+				connection.close(
+					4000,
+					`wantedCollection "${collection}" is not included in server wantedCollections. Please check server config`,
+				);
+			}
 		}
-	};
-	for (const collection of wantedCollections) {
-		const result = connections.get(collection)?.add(connection);
-		if (result == null) {
-			cleanup();
-			connection.close(
-				4000,
-				`wantedCollection "${collection}" is not included in server wantedCollections. Please check server config`,
-			);
-		}
+	} else {
+		// wantedCollectionsが指定されていない場合
+		cleanup = () => {
+			allSubscriber.delete(connection);
+		};
+		allSubscriber.add(connection);
 	}
 	connection.on("close", (code, reason) => {
 		console.log("client disconnect");
 		cleanup();
 	});
-	console.dir(connections, { depth: 1 });
 });
 
 let cursor = -1;
