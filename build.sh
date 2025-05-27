8e67bb582f5eacc002210895cd951a07f80487bb
tomo-x7 <158121497+tomo-x7@users.noreply.github.com>
2025-05-27 21:51:10 +0900

    fix

--- build.sh
+++ build.sh
@@ -12,7 +12,7 @@ NAME=jetstreamproxy_${VERSION}_${NODE_V}_${PLATFORM}_${ARC}
 tsc
 node ./esbuild.js
 node --experimental-sea-config sea-config.json
-cp $(command -v node) tmp/jetstreamproxy
+cp $NODE_PATH tmp/jetstreamproxy
 postject tmp/jetstreamproxy NODE_SEA_BLOB tmp/jetstreamproxy.blob --sentinel-fuse NODE_SEA_FUSE_fce680ab2cc467b6e072b8b5df1996b2 
 mkdir -p dist
 cp tmp/jetstreamproxy dist/jetstreamproxy
