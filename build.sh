fa696eea31aff1435116a3874b0d81576de2b299
tomo-x7 <158121497+tomo-x7@users.noreply.github.com>
2025-05-27 21:34:32 +0900

    ビルドスクリプトを変更

--- build.sh
+++ build.sh
@@ -1,17 +1,19 @@
 #!/bin/bash
-
+NODE_PATH=$1
+if [ -z "$NODE_PATH" ]; then
+  NODE_PATH=$(command -v node)
+fi
 VERSION=`node -p "require('./package.json').version"`
-NODE_V=`node -v`
+NODE_V=`($NODE_PATH -v)`
 NODE_V=node${NODE_V//v/}
-ARC=`uname -m`
-ARC=linux_${ARC}
-NAME=jetstreamproxy_${VERSION}_${NODE_V}_${ARC}
+ARC=`$NODE_PATH --eval="console.log(os.arch())"`
+PLATFORM=`$NODE_PATH --eval="console.log(os.platform())"`
+NAME=jetstreamproxy_${VERSION}_${NODE_V}_${PLATFORM}_${ARC}
 tsc
 node ./esbuild.js
 node --experimental-sea-config sea-config.json
 cp $(command -v node) tmp/jetstreamproxy
 postject tmp/jetstreamproxy NODE_SEA_BLOB tmp/jetstreamproxy.blob --sentinel-fuse NODE_SEA_FUSE_fce680ab2cc467b6e072b8b5df1996b2 
-rm -rf dist
 mkdir -p dist
 cp tmp/jetstreamproxy dist/jetstreamproxy
 tar -caf dist/${NAME}.tar.gz -C tmp jetstreamproxy
